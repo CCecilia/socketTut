7c0804deaf494e332d2571871e6030191db2ce8f
CDoughboy <christian.cecilia1@gmail.com>
2018-01-24 19:25:35 -0500

    init socket io

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const PORT    = process.env.PORT || 8000;
 const express = require('express');
 const app     = express();
 const http    = require('http').Server(app);
-const path      = require('path');
+const path    = require('path');
+const io      = require('socket.io')(http);
 
 // View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,8 +12,14 @@ app.set('view engine', 'pug');
 // Express Setup
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes 
 app.get('/', function(req, res) {
     res.render('index.pug'); 
+});
+
+io.on('connection', function(){
+    console.log('User connected via socket io');
 });
 
 http.listen(PORT, function(){
